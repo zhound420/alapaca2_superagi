42d9764be4f65fd90627caec4799a4250ebd6762
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-08-11 22:40:49 -0700

    Update MonitorTool.py

--- MonitorTool.py
+++ MonitorTool.py
@@ -1,11 +1,10 @@
 
-class MonitorInput(BaseModel):
-    
+class MonitorInput(BaseModel):    
 
-class MonitorTool(BaseTool):
-    name: str = "Monitor Tool"
-    args_schema: Type[BaseModel] = MonitorInput
-    description: str = "Tool for monitor method."
+    class MonitorTool(BaseTool):
+        name: str = "Monitor Tool"
+        args_schema: Type[BaseModel] = MonitorInput
+        description: str = "Tool for monitor method."
 
     def _execute(self, ):
         trader_instance = TraderOriginal()
