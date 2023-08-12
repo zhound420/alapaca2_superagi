8c81e8a6e0d6f2dd40496c137d89fde549e0dca9
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-08-11 22:57:13 -0700

    Update MonitorTool.py

--- MonitorTool.py
+++ MonitorTool.py
@@ -1,7 +1,8 @@
 from superagi.tools.base_tool import BaseTool
 from pydantic import BaseModel, Field
-from typing import Typeclass MonitorInput(BaseModel):    
+from typing import Typeclass 
 
+MonitorInput(BaseModel):    
 class MonitorTool(BaseTool):
     name: str = "Monitor Tool"
     args_schema: Type[BaseModel] = MonitorInput
